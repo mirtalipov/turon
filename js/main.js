bf0542fd46fe34fc6e157ce575817749fed69084
mirtalipov <149838410+mirtalipov@users.noreply.github.com>
2024-01-29 13:01:26 +0500

    i
    i

---
+++ js/main.js
@@ -0,0 +1,66 @@
+document.getElementById('googledoc').addEventListener('click', function () {
+    document.getElementById('selection').innerHTML = 'Выбрано: Инструкции';
+});
+
+document.getElementById('ticket').addEventListener('click', function () {
+    document.getElementById('selection').innerHTML = 'Выбрано: Турон тиккет';
+});
+
+document.getElementById('turon').addEventListener('click', function () {
+    document.getElementById('selection').innerHTML = 'Выбрано: Турон билинг';
+});
+
+document.getElementById('stv1').addEventListener('click', function () {
+    document.getElementById('selection').innerHTML = 'Выбрано: ЦТВ Ташкент';
+});
+
+document.getElementById('stv2').addEventListener('click', function () {
+    document.getElementById('selection').innerHTML = 'Выбрано: ЦТВ Бухара';
+});
+
+document.getElementById('stv3').addEventListener('click', function () {
+    document.getElementById('selection').innerHTML = 'Выбрано: ЦТВ Навои';
+});
+document.getElementById('stv4').addEventListener('click', function () {
+    document.getElementById('selection').innerHTML = 'Выбрано: ЦТВ Заравшон';
+});
+document.getElementById('ktv').addEventListener('click', function () {
+    document.getElementById('selection').innerHTML = 'Выбрано: КТВ';
+});
+document.getElementById('cinerama').addEventListener('click', function () {
+    document.getElementById('selection').innerHTML = 'Выбрано: Синерама админ';
+});
+document.getElementById('Turontelecom').addEventListener('click', function () {
+    document.getElementById('selection').innerHTML = 'Выбрано: Турон телеком';
+});
+document.getElementById('netco').addEventListener('click', function () {
+    document.getElementById('selection').innerHTML = 'Выбрано: Нетко телеком';
+});
+
+document.getElementById('cinerama2').addEventListener('click', function () {
+    document.getElementById('selection').innerHTML = 'Выбрано: Синерама';
+});
+document.getElementById('turon').addEventListener('click', function () {
+    document.getElementById('selection').innerHTML = 'Выбрано: Турон билинг';
+});
+document.getElementById('dialogue').addEventListener('click', function () {
+    document.getElementById('selection').innerHTML = 'Выбрано: скрипт';
+});
+document.getElementById('Инциденты').addEventListener('click', function () {
+    document.getElementById('selection').innerHTML = 'Выбрано: Инциденты';
+});
+document.getElementById('График').addEventListener('click', function () {
+    document.getElementById('selection').innerHTML = 'Выбрано: График работы';
+});
+document.getElementById('допуслугиturon').addEventListener('click', function () {
+    document.getElementById('selection').innerHTML = 'Выбрано: Дополнительные услуги Turon';
+});
+document.getElementById('допуслугиnetco').addEventListener('click', function () {
+    document.getElementById('selection').innerHTML = 'Выбрано: Дополнительные услуги Netco';
+});
+document.getElementById('Dinner').addEventListener('click', function () {
+    document.getElementById('selection').innerHTML = 'Выбрано: Dinner/Sleep time';
+});
+document.getElementById('Обучение').addEventListener('click', function () {
+    document.getElementById('selection').innerHTML = 'Выбрано: Обучение';
+});
