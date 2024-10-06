b700857879ee1702fcf0a19d9080a4d918375ed8
mirtalipov <149838410+mirtalipov@users.noreply.github.com>
2024-10-07 02:34:04 +0500

    Add files via upload

---
+++ Сотрудничество/funcion.js
@@ -0,0 +1,53 @@
+const URL_APP = "https://script.google.com/macros/s/AKfycbwmM3NnAVNLu6LYqWbiUAR6uqzUWANMPzNmRvTUA4stJWZeIxOcvYRNF6X15OI-axSJ/exec"; 
+    
+const form = document.querySelector(".data_abonent");
+
+form.addEventListener("submit", async (ev) => {
+    ev.preventDefault();
+
+    const ссылка = document.querySelector("[name='ссылка']").value.trim();
+    const номер = document.querySelector("[name='номер телефона']").value.trim();
+    const choices = document.querySelector("[name='choices']").value;
+    const комментария = document.querySelector("[name='комментария']").value.trim();
+
+    // Проверка номера телефона на наличие только цифр
+    const phonePattern = /^[0-9]+$/;
+    if (!phonePattern.test(номер)) {
+        alert("Номер телефона должен содержать только цифры.");
+        return; // Прерываем выполнение функции, чтобы не отправлять данные
+    }
+
+    let details = {
+        ссылка,
+        номер,
+        choices,
+        комментария
+    };
+
+    let formBody = [];
+    for (let property in details) {
+        let encodedKey = encodeURIComponent(property);
+        let encodedValue = encodeURIComponent(details[property]);
+        formBody.push(encodedKey + "=" + encodedValue);
+    }
+    formBody = formBody.join("&");
+
+    try {
+        const result = await fetch(URL_APP, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
+            },
+            body: formBody
+        }).then((res) => res.json());
+
+        if (result.type === "success") {
+            alert("Данные успешно отправлены!");
+            form.reset();
+        } else if (result.type === "error") {
+            alert(`Ошибка: ${result.errors}`);
+        }
+    } catch (err) {
+        alert("Ошибка при отправке данных!");
+    }
+});
